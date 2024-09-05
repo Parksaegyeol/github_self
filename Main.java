1f30b6523f5a3fe0cfbb59ed252eba7d25f6c3f5
parksaegyeol <dmsgkrkt1@naver.com>
2024-09-05 17:15:20 +0900

    수저웃정수정

--- Main.java
+++ Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("생성");
+        System.out.println("추가");
+
+
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i);
+
+        }
+
+        
+    }
 }
