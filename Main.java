876280e3d4fee3e285191acd4f12fb3d07f69378
parksaegyeol <dmsgkrkt1@naver.com>
2024-09-05 17:16:40 +0900

    ㅁㅈㅇㅈㅇ

--- Main.java
+++ Main.java
@@ -1,17 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-
-        System.out.println("생성");
-        System.out.println("추가");
-
-
-        int arr[] = new int[5];
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(i);
-
-        }
-
+        System.out.println("처음");
         
     }
 }
